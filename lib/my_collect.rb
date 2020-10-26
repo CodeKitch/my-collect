df15555ca668ef34052d378e43cd650bf7a67909
JaSON <masteraccounthub@gmail.com>
2020-10-26 07:46:29 -0400

    commit tree ff62f38f111739b49b4e27d7150fc4338a28b53e

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+        counter = 0 
+        new_list = [] 
+    while counter < array.length
+       new_ele = yield(array[counter])
+       new_list.push(new_ele)
+        counter += 1 
+    end
+        new_list
+end
